a68b1feb7a053d630482a732da884d18eb8bc27e
Evan <wilbardevan03@gmail.com>
2026-08-26 23:37:35 +0200

    feat: implement Todo component with task management functionality

--- angulartodo/src/app/todo/todo.ts
+++ angulartodo/src/app/todo/todo.ts
@@ -3,7 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-todo',
+  standalone: true,
   styleUrl: './todo.css',
   templateUrl: './todo.html',
 })
-export class Todo {}
+export class Todo {
+  task = [
+    { name: 'Börja med inlämningen', done: true },
+    { namn: 'Göra klart Angular-koden', done: false },
+  ];
+
+  addTask(name: string, inputElement: HTMLInputElement) {
+    if (name.trim() !== '') {
+      this.task.push({ name: name, done: false });
+      inputElement.value = '';
+    }
+  }
+
+  changeStatus(task: any) {
+    task.done = !task.done;
+  }
+}
