7f85b6fda36c6d54af687bd442c7635a7a6ee238
Evan <wilbardevan03@gmail.com>
2026-08-26 20:55:32 +0200

    feat: add About and Todo components with basic structure and styles
    - Created About component with HTML, CSS, and test files.
    - Created Todo component with HTML, CSS, and test files.
    - Updated app component to include routing for About and Todo.
    - Removed placeholder content from app.html and added navigation.

---
+++ angulartodo/src/app/todo/todo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-todo',
+  styleUrl: './todo.css',
+  templateUrl: './todo.html',
+})
+export class Todo {}
