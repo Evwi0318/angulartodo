dd27de2120aac1b8ee97922381ace719804ddb45
Evan <wilbardevan03@gmail.com>
2026-08-26 21:46:11 +0200

    feat: define application routes for Todo and About components

--- angulartodo/src/app/app.routes.ts
+++ angulartodo/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Todo } from './todo/todo';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todo', component: Todo },
+  { path: 'about', component: About },
+  { path: '', redirectTo: '/todo', pathMatch: 'full' },
+];
